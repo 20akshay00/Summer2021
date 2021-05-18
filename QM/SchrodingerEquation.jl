09a18e62ccf316c4fb0f5e39c898da82762002ad
Akshay Shankar <sakshays.2000@gmail.com>
2021-05-18 12:27:29 +0530

    added heat equation analysis

--- QM/SchrodingerEquation.jl
+++ QM/SchrodingerEquation.jl
@@ -27,13 +27,89 @@ pluto-helpbox {
 }
 </style>"
 
+# ╔═╡ e8a5381b-7810-4239-9b36-8ca4efc68f4e
+md"""
+# Simulating the 1-D TDSE
+"""
+
+# ╔═╡ aad53d7a-eeea-4dd8-bf2d-f012d697dc91
+md"""
+$$i\hbar\frac{\partial \psi(x, t)}{\partial t} = \left(-\frac{\hbar^2}{2m} \frac{\partial^2}{\partial x^2} + V(x) \right) \psi(x, t)$$ 
+
+"""
+
+# ╔═╡ f10924c1-3f11-4cda-acb2-02aafc605562
+md"### Crank-Nicholson scheme"
+
+# ╔═╡ 625ad5be-5f13-4bd5-afbf-d56e4d99b598
+md"""
+We can write a forward and backward euler expansion for the time derivative (setting $$\hbar = 1$$):
+
+$$\psi(x, t + \Delta t) = \psi(x, t) - i \cdot \hat{H}\psi(x,t) \cdot \Delta t$$
+
+$$\psi(x, t + \Delta t) = \psi(x, t) - i \cdot \hat{H}\psi(x,t + \Delta t) \cdot \Delta t$$
+
+Taking the mean value of these two expressions, we obtain the crank nicholson scheme:
+
+$$\left(1 + \frac{1}{2} i\hat{H}\Delta t\right)\psi(x, t + \Delta t) = \left(1 - \frac{1}{2} i\hat{H}\Delta t\right)\psi(x, t)$$
+
+This gives us a unitary evolution operator, which will preserve the normalization. The second derivative in $$\hat{H}$$ can be approximated using finite differences:
+
+$$\frac{\partial^2 \psi_j^n}{\partial x^2} = \frac{\psi_{j+1}^n - 2\psi_j^n + \psi_{j-1}^n}{(\Delta x)^2}$$
+
+This can then be written in matrix form like so; 
+
+$$M_1 \psi^{n+1} = M_2 \psi^n \hspace{1cm} \implies \hspace{1cm} \psi^{n+1} = M_1 ^{-1} \cdot M_2 \psi^n$$
+"""
+
+# ╔═╡ 27f27028-83cb-4b44-86af-435f018169d2
+md"""
+$$M_1 = \begin{pmatrix}
+\xi & -\alpha & 0 & ... & ... & ... & 0\\
+-\alpha & \xi & -\alpha & 0 & . &. & .\\
+0 & -\alpha & \xi & -\alpha & 0 & . & .\\
+\vdots & \ddots & \ddots & \ddots & \ddots & \ddots & \vdots\\
+. & . & 0 & -\alpha & \xi & -\alpha & 0\\
+. & . & . & 0 & -\alpha & \xi & -\alpha\\
+0 & ... & ... & ...& 0 & -\alpha & \xi &
+\end{pmatrix}
+
+\hspace{2cm}
+
+M_2 = \begin{pmatrix}
+\gamma & \alpha & 0 & ... & ... & ... & 0\\
+\alpha & \gamma & \alpha & 0 & . &. & .\\
+0 & \alpha & \gamma & \alpha & 0 & . & .\\
+\vdots & \ddots & \ddots & \ddots & \ddots & \ddots & \vdots\\
+. & . & 0 & \alpha & \gamma & \alpha & 0\\
+. & . & . & 0 & \alpha & \gamma & \alpha\\
+0 & ... & ... & ...& 0 & \alpha & \gamma &
+\end{pmatrix}$$
+"""
+
+# ╔═╡ 16911457-303f-4f3a-b64d-5ea72ac3b040
+md"""
+$$\boxed{\alpha = \frac{i\Delta t}{4m(\Delta x)^2}}
+\hspace{2cm}
+\boxed{\xi = 1 + \frac{i\Delta t}{2} \cdot \left ( \frac{1}{m(\Delta x)^2} + V(x)\right)}
+\hspace{2cm}
+\boxed{\gamma = 1 - \frac{i\Delta t}{2} \cdot \left ( \frac{1}{m(\Delta x)^2} + V(x)\right)}$$
+
+"""
+
+# ╔═╡ e9d1f155-19a4-40d2-89e7-3b21b7ef856d
+md"#### Initial Condition"
+
 # ╔═╡ 208cbf8a-954a-11eb-31c8-014c1b072be0
 function initCond(xgrid, sType, μ, σ, p)
 	
     if(sType == 1) #moving gaussian
-       return Complex.(1/(σ*sqrt(2*π))^0.5 * exp.(p*im*xgrid - ((xgrid .- μ) .^2 ./ (4*σ^2))))
+       return @. Complex(1/(σ*sqrt(2*π))^0.5 * exp(p*im*xgrid - ((xgrid - μ)^2/(4*σ^2))))
 	end
 end
+
+# ╔═╡ fa59ac6f-1e55-4bd7-955e-a71703b54a2b
+md"#### Crank-Nicholson evolution"
 
 # ╔═╡ 1fc331e4-954a-11eb-28ba-d17a94789af9
 function schrodingerEquation(dx::Float64, xi::Float64, xf::Float64, m::Int64, dt::Float64, mass::Float64, wavepacket::Tuple{Float64, Float64, Float64}, potential)
@@ -43,7 +119,7 @@ function schrodingerEquation(dx::Float64, xi::Float64, xf::Float64, m::Int64, dt
 	
 	V = potential.(xgrid)
 	
-	#CREATING EVOLUTION MATRIX
+	#CONSTRUCTING THE EVOLUTION MATRIX
 	diag = ones(Complex, n)
 	α = im*dt/(4*mass*dx^2) * diag[2:end]
 	ξ = 1 .+ (im * dt/2) * (1/(mass*dx^2) .+ V)
@@ -64,6 +140,9 @@ function schrodingerEquation(dx::Float64, xi::Float64, xf::Float64, m::Int64, dt
     return (xgrid, V, ψ)
 end
 
+# ╔═╡ 9ae10183-b731-4aef-af7a-f048daf4dc92
+md"#### Potential function"
+
 # ╔═╡ 270699fa-954a-11eb-3e5b-f1dbdaca0fad
 function V(x)
 	return (x > -5.0 && x < 5.0) ? 2.5 : 0.0
@@ -71,8 +150,11 @@ function V(x)
 	#return 0.05*x^2
 end
 
+# ╔═╡ af6758f1-302c-401d-9aac-77f8019eea02
+md"#### Plotting"
+
 # ╔═╡ 5f14351a-3c56-4d0c-9a79-0d71cbc8eef9
-function plotSchrodinger(xgrid, V, ψ, m)
+function plotProbabilityDensity(xgrid, V, ψ, m)
 	#GIF CREATION
     anim = @gif for j = 1:10:m
 		plot(xgrid, abs2.(ψ[:, j]), ylim = (-0.05, 0.5), label = "")
@@ -80,22 +162,51 @@ function plotSchrodinger(xgrid, V, ψ, m)
     end
 end
 
-# ╔═╡ 2344c2b1-280a-46bf-9e82-c7a15db18ead
-m = 2000
+# ╔═╡ d96bc693-7693-4f2e-b119-108bab19cc8b
+function plotRealImg(xgrid, V, ψ, m)
+    anim = @gif for j = 1:10:m
+		plot(xgrid, V, color = :black, label = "", ylim = (-0.5, 0.5))
+		plot!(xgrid, abs2.(ψ[:, j]), color = :black, label = "|ψ|^2")
+		plot!(xgrid, real.(ψ[:, j]), color = :red, label = "Re(ψ)")
+		plot!(xgrid, imag.(ψ[:, j]), color = :blue, label = "Im(ψ)")
+	end
+end
+
+# ╔═╡ 2aa523ef-178c-4c36-8c1c-a67af7e92ed8
+md"## Simulation results"
+
+# ╔═╡ a32b0c6a-895f-4584-90ba-5f17a34a26f2
+nSteps = 2250
 
 # ╔═╡ b15fff98-22f9-4689-957f-348dbd5dd02d
-data = schrodingerEquation(0.01, -35.0, 35.0, m, 0.025, 5.0, (-25.0, 2.0, 6.0), V);
+data = schrodingerEquation(0.01, -40.0, 40.0, nSteps, 0.025, 5.0, (-25.0, 2.0, 6.0), V);
 
 # ╔═╡ a33fa264-6920-459b-964b-88b453600800
-plotSchrodinger(data..., m)
+plotProbabilityDensity(data..., nSteps)
+
+# ╔═╡ ca250f60-9b2f-4784-8353-5bc343b1afcd
+plotRealImg(data..., nSteps)
 
 # ╔═╡ Cell order:
 # ╟─4dce2ef1-30dd-45db-90f4-c3e8597089a6
-# ╠═c8576284-9549-11eb-09a9-49c73a096836
+# ╟─c8576284-9549-11eb-09a9-49c73a096836
+# ╟─e8a5381b-7810-4239-9b36-8ca4efc68f4e
+# ╟─aad53d7a-eeea-4dd8-bf2d-f012d697dc91
+# ╟─f10924c1-3f11-4cda-acb2-02aafc605562
+# ╟─625ad5be-5f13-4bd5-afbf-d56e4d99b598
+# ╟─27f27028-83cb-4b44-86af-435f018169d2
+# ╟─16911457-303f-4f3a-b64d-5ea72ac3b040
+# ╟─e9d1f155-19a4-40d2-89e7-3b21b7ef856d
 # ╠═208cbf8a-954a-11eb-31c8-014c1b072be0
+# ╟─fa59ac6f-1e55-4bd7-955e-a71703b54a2b
 # ╠═1fc331e4-954a-11eb-28ba-d17a94789af9
+# ╟─9ae10183-b731-4aef-af7a-f048daf4dc92
 # ╠═270699fa-954a-11eb-3e5b-f1dbdaca0fad
+# ╟─af6758f1-302c-401d-9aac-77f8019eea02
 # ╠═5f14351a-3c56-4d0c-9a79-0d71cbc8eef9
+# ╠═d96bc693-7693-4f2e-b119-108bab19cc8b
+# ╟─2aa523ef-178c-4c36-8c1c-a67af7e92ed8
+# ╠═a32b0c6a-895f-4584-90ba-5f17a34a26f2
 # ╠═b15fff98-22f9-4689-957f-348dbd5dd02d
-# ╠═2344c2b1-280a-46bf-9e82-c7a15db18ead
 # ╠═a33fa264-6920-459b-964b-88b453600800
+# ╠═ca250f60-9b2f-4784-8353-5bc343b1afcd
---
+++ QM/TDSE_Heat.jl
@@ -0,0 +1,160 @@
+### A Pluto.jl notebook ###
+# v0.14.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 7590d87f-ecd6-46e0-9931-a0febbdebc14
+begin
+	using PlutoUI
+	using Plots
+	using SparseArrays
+    using LinearAlgebra
+	using Statistics
+end
+
+# ╔═╡ 1ef0df5e-b79f-11eb-3dc2-cf714ea96197
+html"<style>
+main {
+    max-width:75%;
+    padding: unset;
+    margin-right: unset !important;
+    margin-bottom: 20px;
+    align-self: center !important;
+}
+pluto-helpbox {
+    visibility: hidden;
+}
+</style>"
+
+# ╔═╡ 81a30c38-4d61-41ae-a654-ebfdf59e7a29
+md"## Investigating the relation between TDSE and Heat equation (1D)"
+
+# ╔═╡ 52d10bb6-d20d-4b39-b9e9-d9460db55f0b
+md""" 
+The TDSE in 1D is given by 
+
+$$i\hbar\frac{\partial \psi(x, \ t)}{\partial t} = \left(-\frac{\hbar^2}{2m} \frac{\partial^2}{\partial x^2} + V(x) \right) \psi(x, \ t)$$ 
+
+If we perform a wick rotation ($$t \to i\tau$$), the equation now resemble a heat/diffusion equation:
+
+$$\frac{\partial \psi(x, \ i\tau)}{\partial \tau} = \left(-\frac{\hbar}{2m} \frac{\partial^2}{\partial x^2} + \frac{1}{\hbar}V(x) \right) \psi(x, \ i\tau)
+\hspace{1cm}
+\longleftrightarrow
+\hspace{1cm}
+\frac{\partial \nu(x, \ t)}{\partial t} = \alpha \frac{\partial^2}{\partial x^2} \nu(x, \ t)$$
+"""
+
+# ╔═╡ 59691809-7282-46ec-b428-cd8be26d20dd
+md"#### Initial Condition"
+
+# ╔═╡ 6a9478e7-1c81-47e5-9266-2d0ec7cbec41
+function initCond(xgrid, sType, μ, σ, p)
+	
+    if(sType == 1) #moving gaussian
+       return @. Complex(1/(σ*sqrt(2*π))^0.5 * exp(p*im*xgrid - ((xgrid - μ)^2/(4*σ^2))))
+	end
+end
+
+# ╔═╡ baf46fd6-511d-4996-acf0-7f4ccf5aef3f
+md"#### Crank-Nicholson scheme"
+
+# ╔═╡ 5b25efd6-2392-45ee-b807-37f732b87021
+function schrodingerEquationImaginary(dx::Float64, xi::Float64, xf::Float64, m::Int64, dt::Float64, mass::Float64, wavepacket::Tuple{Float64, Float64, Float64}, potential)
+    
+    xgrid = collect(xi:dx:xf) #discretized spatial grid
+    n = length(xgrid)
+	
+	V = potential.(xgrid)
+	
+	#CREATING EVOLUTION MATRIX
+	diag = ones(Complex, n)
+	α = dt/(4*mass*dx^2) * diag[2:end]
+	ξ = Complex.(1 .+ (dt/2) * (1/(mass*dx^2) .+ V))
+	γ = Complex.(1 .- (dt/2) * (1/(mass*dx^2) .+ V))
+	
+    M₁ = LinearAlgebra.SymTridiagonal(ξ,-α)
+    M₂ = LinearAlgebra.SymTridiagonal(γ, α)
+	
+    #INITIAL CONDITIONS
+	ψ = zeros(Complex, (n, m))
+    ψ[:, 1] = initCond(xgrid, 1, wavepacket...)
+    
+	#TIME EVOLUTION LOOP
+    for j in 1:(m-1)
+		ψ[:, j+1] = M₁\(M₂*ψ[:, j])
+    	
+		#normalizing
+		ψ[:, j+1] /= sqrt(dx * sum(abs2.(ψ[:, j+1])))
+	end
+    
+    return (xgrid, V, ψ)
+end
+
+# ╔═╡ 00656461-1813-4a12-aa1b-53770a8c16a4
+md"#### Plotting"
+
+# ╔═╡ 039cd28b-8656-4390-9d08-90a31fb56f5e
+function plotProbabilityDensity(xgrid, V, ψ, m)
+	#GIF CREATION
+    anim = @gif for j = 1:10:m
+		plot(xgrid, abs2.(ψ[:, j]), ylim = (-0.05, 0.5), label = "|ψ|^2")
+		plot!(xgrid, V, color = :black, label = "V(x)")
+    end
+end
+
+# ╔═╡ 02787f48-687c-4da6-85c2-f9edb4e02173
+md"## Simulation results"
+
+# ╔═╡ a2784ce9-c818-40c3-8625-a7a5575f1513
+md"##### Free particle; $$V(x) = 0$$"
+
+# ╔═╡ c0588237-68bc-4982-9479-a4d104115edb
+begin
+	dataImag1 = schrodingerEquationImaginary(0.01, -30.0, 30.0, 1500, 0.025, 0.5, (0.0, 1.0, 0.0), (x) -> 0.0);
+	plotProbabilityDensity(dataImag1..., 1500);
+end
+
+# ╔═╡ 259912ca-25bc-447c-830c-d41c0c5a5ff3
+md"##### Harmonic oscillator; $$V(x) = \frac{1}{2}k x^2$$"
+
+# ╔═╡ 967a285d-db6f-4896-a38b-5d8bac787b66
+begin
+	dataImag = schrodingerEquationImaginary(0.01, -15.0, 15.0, 1000, 0.025, 0.5, (-10.0, 1.0, 0.0), (x) -> 0.01*x^2);
+	plotProbabilityDensity(dataImag..., 1000);
+end
+
+# ╔═╡ 698a1b09-93bb-4daa-8d03-9f791e64d37e
+md"##### Step potentials inside an infinite well"
+
+# ╔═╡ 020991f3-9f73-4d5f-b063-33ae0037abbd
+begin
+	dataImag2 = schrodingerEquationImaginary(0.01, -5.0, 5.0, 1000, 0.025, 0.5, (-2.0, 1.0, 0.0), (x) -> (x < 0.0) ? 0 : 0.3);
+	plotProbabilityDensity(dataImag2..., 1000);
+end
+
+# ╔═╡ 8045b71f-f8ef-4a27-8111-94ccc4a7f6e9
+begin
+	dataImag3 = schrodingerEquationImaginary(0.01, -5.0, 5.0, 1500, 0.025, 0.5, (-2.0, 1.0, 0.0), (x) -> (x > -1.0 && x < 1.0) ? 0.4 : 0.0);
+	plotProbabilityDensity(dataImag3..., 1500);
+end
+
+# ╔═╡ Cell order:
+# ╟─1ef0df5e-b79f-11eb-3dc2-cf714ea96197
+# ╟─7590d87f-ecd6-46e0-9931-a0febbdebc14
+# ╟─81a30c38-4d61-41ae-a654-ebfdf59e7a29
+# ╟─52d10bb6-d20d-4b39-b9e9-d9460db55f0b
+# ╟─59691809-7282-46ec-b428-cd8be26d20dd
+# ╠═6a9478e7-1c81-47e5-9266-2d0ec7cbec41
+# ╟─baf46fd6-511d-4996-acf0-7f4ccf5aef3f
+# ╠═5b25efd6-2392-45ee-b807-37f732b87021
+# ╟─00656461-1813-4a12-aa1b-53770a8c16a4
+# ╠═039cd28b-8656-4390-9d08-90a31fb56f5e
+# ╟─02787f48-687c-4da6-85c2-f9edb4e02173
+# ╟─a2784ce9-c818-40c3-8625-a7a5575f1513
+# ╠═c0588237-68bc-4982-9479-a4d104115edb
+# ╟─259912ca-25bc-447c-830c-d41c0c5a5ff3
+# ╠═967a285d-db6f-4896-a38b-5d8bac787b66
+# ╟─698a1b09-93bb-4daa-8d03-9f791e64d37e
+# ╠═020991f3-9f73-4d5f-b063-33ae0037abbd
+# ╠═8045b71f-f8ef-4a27-8111-94ccc4a7f6e9
